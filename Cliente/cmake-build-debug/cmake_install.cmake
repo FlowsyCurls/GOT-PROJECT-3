b413f87517cbbd576d3c7c813470c8b67e87fb6b
Shakime R. Sparks <jeykimers.johnson@gmail.com>
2020-08-13 16:51:31 -0600

    [Added] Se agrega Rest-Api.

--- Cliente/cmake-build-debug/cmake_install.cmake
+++ Cliente/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/heutlett/GOT-PROJECT-3/Cliente
+# Install script for directory: /home/jeykime/Documents/GOT-PROJECT-3/Cliente
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/heutlett/GOT-PROJECT-3/Cliente/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/jeykime/Documents/GOT-PROJECT-3/Cliente/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
---
+++ Rest-Api/db/database.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE IF NOT EXISTS got;
+
+USE got;
+
+CREATE TABLE repository (
+  id INT(11) NOT NULL AUTO_INCREMENT,
+  name VARCHAR(45) DEFAULT NULL,
+  PRIMARY KEY(id)
+);
+
+DESCRIBE repository;
+
+INSERT INTO repository values 
+  (1, 'Proyecto_1'),
+  (2, 'Proyecto_2'),
+  (3, 'Proyecto_3';
+
+SELECT * FROM repository;
