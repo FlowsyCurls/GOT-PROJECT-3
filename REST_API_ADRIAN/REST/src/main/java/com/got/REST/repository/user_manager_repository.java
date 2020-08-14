74fdf9dc95b77c4c0742f4fc6150e872a9e622ca
heutlett <heutlett@github.com>
2020-08-13 20:45:12 -0600

    Se agrega el rest api en eclipse

---
+++ REST_API_ADRIAN/REST/src/main/java/com/got/REST/repository/user_manager_repository.java
@@ -0,0 +1,9 @@
+package com.got.REST.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.got.REST.model.user_manager;
+
+public interface user_manager_repository extends JpaRepository<user_manager, Integer>{
+
+}
