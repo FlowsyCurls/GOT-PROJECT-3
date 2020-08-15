d4fa283b98a751c5a7aa920857e3226fb743b6ea
Shakime R. Sparks <jeykimers.johnson@gmail.com>
2020-08-15 01:43:20 -0600

    [Added] Add Commit Command in Server

--- REST_API_ADRIAN/demo/src/main/java/com/got/REST/controllers/CommandController.java
+++ REST_API_ADRIAN/demo/src/main/java/com/got/REST/controllers/CommandController.java
@@ -2,6 +2,9 @@ package com.got.REST.controllers;
 
 import java.util.List;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,11 +14,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.got.REST.models.Commit;
 import com.got.REST.models.CommitEntityJSON;
 import com.got.REST.models.CommitJSON;
 import com.got.REST.models.File;
 import com.got.REST.models.Repository;
 import com.got.REST.services.IFileService;
+import com.got.REST.services.ICommitService;
 import com.got.REST.services.IRepositoryService;
 
 @RestController
@@ -25,87 +30,92 @@ public class CommandController {
 	IRepositoryService repositoryService;
 	
 	@Autowired
+	ICommitService commitService;
+	
+	@Autowired
 	IFileService fileService;
 	
+	
 	private File getFile(long id) {
-		
 		List<File> lista = fileService.getAll();
-		
 		for(int i = 0; i < lista.size(); i++) { 
 			if(lista.get(i).getIdFile() == id) { 
-				
 				return lista.get(i);
-				
 			}
 		}
-		
 		return null;
 	}
 	
 	private Repository getRepository(long id) { 
 		List<Repository> lista = repositoryService.getAll();
-		
 		for(int i = 0; i < lista.size(); i++) { 
-			if(lista.get(i).getId() == id) { 
-				
+			if(lista.get(i).getId() == id) {
 				return lista.get(i);
-				
 			}
 		}
-		
+		return null;
+	}
+	
+	private Repository getRepositoryByName(String name) { 
+		List<Repository> lista = repositoryService.getAll();
+		for(int i = 0; i < lista.size(); i++) { 
+			if(lista.get(i).getName().contains(name)) { 
+				return lista.get(i);
+			}
+		}
+		return null;
+	}
+	
+	
+	private Commit getCommit(Commit commit) {
+		List<Commit> lista = commitService.getAll();
+		for(int i = 0; i < lista.size(); i++) {
+			Commit tmp = lista.get(i);
+			if(tmp.getMessage().contains(commit.getMessage())){ 
+				return tmp;
+			}
+		}
 		return null;
 	}
 	
 	
 	@GetMapping("/getFile/{id}")
-	public String getFileInfo(@PathVariable(value = "id") long id){
-		
-		
+	public String getFileInfo(@PathVariable(value = "id") long id){	
 		File file = getFile(id);
 		Repository repository = getRepository(id);
-		
 		String s = "id: " + repository.getId() + "Repository name: " + repository.getName();
-		
-		
 		return s;
-		
-		
 	}
 	
 	@PostMapping("/newCommit")
-	public @ResponseBody String add(@RequestBody CommitJSON commit) {
-		
-		System.out.println(commit.getRepositoryName());
-		
-		//Tiene que agregar un nuevo commit que debe tener date y idRepository
-		//Tiene que agregar la lista de archivos que tiene commitJson y ponerles sus atributos
-		
-		
-		
-		
-		/*
-		List<Repository> listOfRepositories = commitService.getAll();
-		
-		for(int i = 0; i < listOfRepositories.size(); i++) { 
+	public @ResponseBody String add(@RequestBody CommitJSON commitJson) {
+		Repository repository = getRepositoryByName(commitJson.getRepositoryName());
+		// Create Commit.
+		String date = "21-10-00";
+		System.out.println("LLega");
+		Commit commit = new Commit(date, commitJson.getMessage(), (int)repository.getId());
+		System.out.println("Pasa new");
+		commitService.post(commit);
+		System.out.println("Se agrega commit");
+
+		commit = getCommit(commit);
+		// Create Files.
+		for(int i = 0; i < commitJson.getFileList().size(); i++) {
 			
-			if(listOfRepositories.get(i).getName().contains(commit.getName())) { 
-				
-				return "Ya existe un repositorio con este nombre"; //No se agrega a la tabla porque ya existe
-				
-			}
-			
+			File file = new File(date, (int)commit.getIdCommit(), commitJson.getFileList().get(i).getHuffmanCode());
+			fileService.post(file);
 		}
-		
-		repositoryService.post(commit);
-		*/
-		return "Saved";
+		return String.valueOf(commit.getIdCommit());
 	}
+	
 	/*
 	@PutMapping("/Repository")
 	public void update(Repository repository, long id) {
 		repositoryService.put(repository, id);
 	}
 	*/
+
+
 	
 	
 }
