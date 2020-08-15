853d58c962cee38c517f5d118591887e5bfe2218
Shakime R. Sparks <jeykimers.johnson@gmail.com>
2020-08-14 19:10:48 -0600

    [Update] Rest-Api vscode

--- Rest-Api/db/database.sql
+++ Rest-Api/db/database.sql
@@ -1,18 +1,45 @@
 CREATE DATABASE IF NOT EXISTS got;
-
 USE got;
 
-CREATE TABLE repository (
-  id INT(11) NOT NULL AUTO_INCREMENT,
-  name VARCHAR(45) DEFAULT NULL,
-  PRIMARY KEY(id)
+CREATE TABLE `Commit` (
+  `ID` int not null auto_increment,
+  `Message` varchar(20) not null,
+  `Date` datetime default null,
+  PRIMARY KEY (`ID`)
 );
 
-DESCRIBE repository;
+CREATE TABLE `Repository` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `Name` VARCHAR(20) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+);
 
-INSERT INTO repository values 
+CREATE TABLE `File` (
+  `ID` int not null auto_increment,
+  `Path` varchar(124) default null,
+  `Name` varchar(20) not null,
+  `Data` text,
+  `RepositoryID` int default null,
+  `CommitID` int default null,
+  PRIMARY KEY (`ID`),
+  CONSTRAINT `CommitID` FOREIGN KEY (`CommitID`) REFERENCES `Commit` (`ID`),
+  CONSTRAINT `RepositoryID` FOREIGN KEY (`RepositoryID`) REFERENCES `Repository` (`ID`)
+);
+
+
+INSERT INTO `Repository` values 
   (1, 'Proyecto_1'),
   (2, 'Proyecto_2'),
   (3, 'Proyecto_3');
 
-SELECT * FROM repository;
+INSERT INTO `Commit` values 
+  (1, 'Se agrega readme.md','2015-11-05 14:29:36'),
+  (2, 'Se agrega gitignore.','2023-01-21 09:20:00'),
+  (3, 'Se arreglan buggs','2000-10-31 15:50:15');
+  
+INSERT INTO `File` values 
+  (1, '/home/jeykime/Documents','GOT','01010101',1,1),
+  (1, '/home/adrian/Downloads','AIRWAR','01010101',2,3),
+  (1, '/home/shakime/Desktop','SCRABBLE','01010101',1,2);
+
+SELECT * FROM File INNER JOIN Commit on Commit.ID = CURSO.Escuela INNER JOIN PROFESOR  on CURSO.Profesor = PROFESOR.ID;
--- Rest-Api/src/routes/repository.js
+++ Rest-Api/src/routes/repository.js
@@ -1,27 +1,45 @@
-const express = require('express');
+const express = require('express'); // call express
 const router = express.Router();
 
 const mysqlConnection  = require('../database.js');
 
 
+//=======================================            =======================================//
+//======================================= REPOSITORY =======================================//
+//=======================================            =======================================//
+
+
 // GET all Repositories
-router.get('/', (req, res) => {
-  mysqlConnection.query('SELECT * FROM repository', (err, rows, fields) => {
+router.get('/repos', (req, res) => {
+  mysqlConnection.query('SELECT * FROM Repository', (err, rows, fields) => {
     if(!err) {
       res.json(rows);
+      // console.log(rows);
     } else {
       console.log(err);
     }
   });
 });
 
-
-// GET Repository
-router.get('/:id', (req, res) => {
-  const { id } = req.params; 
-  mysqlConnection.query('SELECT * FROM repository WHERE id = ?', [id], (err, rows, fields) => {
+// GET specific Repository
+router.get('/repo/:ID', (req, res) => {
+  const { ID } = req.params; 
+  mysqlConnection.query('SELECT * FROM Repository WHERE id = ?', [ID], (err, rows, fields) => {
     if (!err) {
       res.json(rows[0]);
+      // console.log(rows[0]);
+    } else {
+      console.log(err);
+    }
+  });
+});
+
+// DELETE Repository
+router.delete('/repo/:ID', (req, res) => {
+  const { ID } = req.params;
+  mysqlConnection.query('DELETE FROM Repository WHERE ID = ?', [ID], (err, rows, fields) => {
+    if(!err) {
+      res.json({status: 'Repository Deleted'});
     } else {
       console.log(err);
     }
@@ -30,14 +48,11 @@ router.get('/:id', (req, res) => {
 
 
 // INSERT Repository
-router.post('/', (req, res) => {
-  const {id, name} = req.body;
-  console.log('Inserting: ', id, name);
-  const query = `
-  SET @id = ?;
-  SET @name = ?;
-  CALL repositoryAddOrEdit(@id, @name);
-`;  mysqlConnection.query(query, [id, name], (err, rows, fields) => {
+router.post('/repo', (req, res) => {
+  const {ID, Name} = req.body;
+  // console.log('Inserting: ', ID, Name);
+  const query = `INSERT INTO Repository VALUES(?,?)`;
+  mysqlConnection.query(query, [ID, Name], (err, rows, fields) => {
     if(!err) {
       res.json({status: 'Repository Saved'});
     } else {
@@ -48,15 +63,12 @@ router.post('/', (req, res) => {
 
 
 // UPDATE Repository
-router.put('/:id', (req, res) => {
-  const { name } = req.body;
-  const { id } = req.params;
-  const query = `
-  SET @id = ?;
-  SET @name = ?;
-  CALL repositoryAddOrEdit(@id, @name);
-`;
-  mysqlConnection.query(query, [id, name], (err, rows, fields) => {
+router.put('/repo/:ID', (req, res) => {
+  const { Name } = req.body;
+  const { ID } = req.params;
+  // console.log('Updating: ', ID, Name);
+  const query = `UPDATE Repository SET Name = ? WHERE id = ?`;
+  mysqlConnection.query(query, [Name, ID], (err, rows, fields) => {
     if(!err) {
       res.json({status: 'Repository Updated'});
     } else {
@@ -66,13 +78,139 @@ router.put('/:id', (req, res) => {
 });
 
 
-// DELETE Repository
-router.delete('/:id', (req, res) => {
-  const { id } = req.params;
-  mysqlConnection.query('DELETE FROM repository WHERE id = ?', [id], (err, rows, fields) => {
+
+
+
+//=======================================        =======================================//
+//======================================= COMMIT =======================================//
+//=======================================        =======================================//
+
+
+// GET all Commits
+router.get('/commits', (req, res) => {
+  mysqlConnection.query('SELECT * FROM Commit', (err, rows, fields) => {
     if(!err) {
-      res.json({status: 'Repository Deleted'});
+      res.json(rows);
+      // console.log(rows);
     } else {
+      console.log(err);
+    }
+  });
+});
+
+// GET specific Commit
+router.get('/commit/:comID', (req, res) => {
+  const { comID } = req.params; 
+  mysqlConnection.query('SELECT * FROM Commit WHERE id = ?', [comID], (err, rows, fields) => {
+    if (!err) {
+      res.json(rows[0]);
+      // console.log(rows[0]);
+    } else {
+      console.log(err);
+    }
+  });
+});
+
+// DELETE Commit
+router.delete('/commit/:comID', (req, res) => {
+  const { comID } = req.params;
+  mysqlConnection.query('DELETE FROM Repository WHERE id = ?', [comID], (err, rows, fields) => {
+    if(!err) {
+      res.json({status: 'Commit Deleted'});
+    } else {
+      console.log(err);
+    }
+  });
+});
+
+// INSERT Commit
+router.post('/commit', (req, res) => {
+  const {ID, Message, Date} = req.body;
+  // console.log('Inserting: ', ID, Message, Date);
+  const query = `INSERT INTO Commit VALUES(?,?,?)`;
+  mysqlConnection.query(query, [ID, Message, Date], (err, rows, fields) => {
+    if(!err) {
+      res.json({status: 'Commit Saved'});
+    } else {
+      console.log(err);
+    }
+  });
+});
+
+// Update Commit
+router.put('/commit/:commitID', (req, res) => {
+  const { commitID } = req.params;
+  const {Message, Date} = req.body;
+  // console.log('Updating: ', commitID, Message, Date);
+  const query = `UPDATE Commit SET Message = ?, Date = ? WHERE id = ?`;
+  mysqlConnection.query(query, [Message, Date, commitID], (err, rows, fields) => {
+    if(!err) {
+      res.json({status: 'Commit Update'});
+    } else {
+      console.log(err);
+    }
+  });
+});
+
+//=======================================      ======================================//
+//======================================= FILE =======================================//
+//=======================================      =======================================//
+
+// GET all File
+router.get('/files', (req, res) => {
+// router.get('/repo/:repoID/commit/:comID/file', (req, res) => {
+  const { comID } = req.params;
+  const { repoID } = req.params;
+  mysqlConnection.query('SELECT File.* FROM File INNER JOIN Commit on Commit.ID = File.CommitID INNER JOIN Repository on Repository.ID = File.RepositoryID', [comID,repoID], (err, rows, fields) => {
+    if(!err) {
+      res.json(rows);
+      // console.log(rows);
+    } else {
+      console.log(err);
+    }
+  });
+});
+
+
+// GET specific File
+router.get('/repo/:repoID/commit/:comID/file/:fileID', (req, res) => {
+  const { comID } = req.params;
+  const { repoID } = req.params;
+  const { fileID } = req.params;
+  mysqlConnection.query('SELECT File.* FROM File INNER JOIN Commit on Commit.ID = ? INNER JOIN Repository on Repository.ID = ? WHERE `File`.id = ?', [comID, repoID, fileID], (err, rows, fields) => {
+    if(!err) {
+      res.json(rows);
+      console.log(rows);
+    } else {
+      console.log(err);
+    }
+  });
+});
+
+
+// DELETE File
+router.delete('/repo/:repoID/commit/:comID/file/:fileID', (req, res) => {
+  const { comID, repoID, fileID} = req.params;
+  mysqlConnection.query('DELETE File.* FROM File INNER JOIN Commit on Commit.ID = ? INNER JOIN Repository on Repository.ID = ? WHERE `File`.id = ?', [comID, repoID, fileID], (err, rows, fields) => {
+    if(!err) {
+      res.json({status: 'File Deleted'});
+    } else {
+      console.log(err);
+    }
+  });
+});
+
+
+// INSERT File
+router.post('/repo/:repoID/commit/:comID/file', (req, res) => {
+  const {ID, Path, Name, Data} = req.body;
+  const { comID, repoID } = req.params;
+  console.log('Inserting: ', ID, Path, Name, Data, comID, repoID);
+  const query = `INSERT INTO Commit VALUES(?,?,?,?,?,?)`;
+  mysqlConnection.query(query, [ID, Path, Name, Data, repoID, comID], (err, rows, fields) => {
+    if(!err) {
+      res.json({status: 'File Saved'});
+    } else { 
       console.log(err);
     }
   });
