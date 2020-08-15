cffa77842a257106fe8c168a74a80b8fe60a7252
heutlett <heutlett@github.com>
2020-08-14 20:35:35 -0600

    Agregando al rest api

--- REST_API_ADRIAN/demo/src/main/java/com/got/REST/controllers/RepositoryController.java
+++ REST_API_ADRIAN/demo/src/main/java/com/got/REST/controllers/RepositoryController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.got.REST.models.Repository;
@@ -17,6 +19,7 @@ public class RepositoryController{
 
 	@Autowired
 	IRepositoryService repositoryService;
+	
 	
 	@GetMapping("/Repositories")
 	public List<Repository> getAllRepositories(){
@@ -29,8 +32,27 @@ public class RepositoryController{
 	}
 	
 	@PostMapping("/Repository")
-	public void add(Repository repository) {
-		repositoryService.post(repository);
+
+	public @ResponseBody String add(String repository) {
+		
+		System.out.println(repository);
+		
+		/*
+		List<Repository> listOfRepositories = repositoryService.getAll();
+		
+		for(int i = 0; i < listOfRepositories.size(); i++) { 
+			
+			if(listOfRepositories.get(i).getName().contains(repository.getName())) { 
+				
+				return; //No se agrega a la tabla porque ya existe
+				
+			}
+			
+		}
+		
+		repositoryService.post(repository);*/
+		
+		return "Saved";
 	}
 	
 	@PutMapping("/Repository")
